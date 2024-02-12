2c37b9ca3cb3dbad2c7f99391e82144199e2c390
Pyi Soe <158580215+PSNISC@users.noreply.github.com>
2024-02-12 15:10:37 +0630

    Update install.py

--- install.py
+++ install.py
@@ -76,7 +76,7 @@ def install():
 
     try:
 
-        run( "sudo touch ./deaups && sudo echo '#! /bin/bash\\npython3 /usr/local/bin/install.py \"$@\"' > ./deaups && sudo chmod +x ./deaups && sudo mv ./deaups /usr/local/bin/ && sudo mv ./install.py /usr/local/bin/" )
+        run( "sudo touch ./deaups && sudo echo -e '#! /bin/bash\\npython3 /usr/local/bin/install.py \"$@\"' > ./deaups && sudo chmod +x ./deaups && sudo mv ./deaups /usr/local/bin/ && sudo mv ./install.py /usr/local/bin/" )
 
     except:
 
